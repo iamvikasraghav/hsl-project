2bbaa2a972d3a5ffe5c93a6ec12bbad918075d62
unknown <vikasraghav9654@gmail.com>
2020-12-23 18:30:46 +0530

    Creating Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from "./pages/Home.js";
+import Rooms from "./pages/Rooms.js";
+import SingleRoom from "./pages/SingleRoom.js";
+import Error from "./pages/Error.js";
+import  {Route, Switch} from 'react-router-dom';
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <> Hello from app </>
+  return (
+    <>
+    <Navbar />
     
-;
+    <Switch>
+              
+    <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+   
+    </Switch>
+    
+       
+    </>
+  );
 }
 
 export default App;
